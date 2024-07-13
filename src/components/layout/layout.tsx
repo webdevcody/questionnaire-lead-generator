e1e37b4d736af238846b0e9fbd717d3fe8c53cab
Cody Seibert <webdevcody@gmail.com>
2024-07-12 23:27:35 -0400

    remove live reload

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -13,18 +13,15 @@ export function Layout({
     <html data-theme="luxury">
       <head>
         <title>{title}</title>
-        {/* <script
-          src="https://unpkg.com/htmx.org@2.0.1"
-          integrity="sha384-QWGpdj554B4ETpJJC9z+ZHJcA/i59TyjxEPXiiUgN2WmTyV5OEZWCD6gQhgkdpB/"
-          crossorigin="anonymous"
-        ></script> */}
         <script
           src="https://unpkg.com/htmx.org@2.0.1/dist/htmx.js"
           integrity="sha384-gpIh5aLQ0qmX8kZdyhsd6jA24uKLkqIr1WAGtantR4KsS97l/NRBvh8/8OYGThAf"
           crossorigin="anonymous"
         ></script>
         <script src="https://unpkg.com/hyperscript.org@0.9.9"></script>
-        <script src="/static/livereload.js"></script>
+        {process.env.NODE_ENV !== "production" && (
+          <script src="/static/livereload.js"></script>
+        )}
         <link href="/static/output.css" rel="stylesheet" />
       </head>
       <body>
